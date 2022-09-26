b7c02df782903b1e629d989ad7d95f8b071d91a0
DenisOuma <dennisouma38@gmail.com>
2022-09-26 21:11:16 +0300

    filter category

--- src/components/App.js
+++ src/components/App.js
@@ -9,16 +9,32 @@ console.log({ CATEGORIES, TASKS });
 
 function App() {
 	const [tasks, setTasks] = React.useState(TASKS);
+	const [category, setCategory] = React.useState(CATEGORIES);
+	const [newTask, setNewTask] = React.useState("");
 	const handleDeleteTasks = (task) => {
-		console.log(task);
 		let newTasks = tasks.filter((t) => t.text !== task);
 		setTasks(newTasks);
 	};
+	const handleFilterTasks = (category) => {
+		if (category === "All") {
+			setTasks(TASKS);
+		} else {
+			let newTasks = TASKS.filter((t) => t.category === category);
+			setTasks(newTasks);
+		}
+	};
+	const handleAddTask = (newTask) => {
+		setNewTask([...tasks, newTask]);
+	};
+
 	return (
 		<div className="App">
 			<h2>My tasks</h2>
-			<CategoryFilter />
-			<NewTaskForm />
+			<CategoryFilter
+				category={category}
+				handleFilterTasks={handleFilterTasks}
+			/>
+			<NewTaskForm newTask={newTask} handleAddTask={handleAddTask} />
 			<TaskList tasks={tasks} handleDeleteTasks={handleDeleteTasks} />
 		</div>
 	);
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function CategoryFilter() {
-  return (
-    <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
-    </div>
-  );
+function CategoryFilter({ category, handleFilterTasks }) {
+	return (
+		<div className="categories">
+			<h5>Category filters</h5>
+			{/* render <button> elements for each category here */}
+			{category.map((cat) => (
+				<button key={cat} onClick={() => handleFilterTasks(cat)}>
+					{cat}
+				</button>
+			))}
+		</div>
+	);
 }
 
 export default CategoryFilter;
